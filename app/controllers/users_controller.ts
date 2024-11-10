748e4230a32cb7b3a6c5879e38338d160a0b8ff0
mar1n <30738216+mar1n@users.noreply.github.com>
2024-11-10 19:15:53 +0000

    fix model for retrive data from db

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -2,10 +2,10 @@ import type { HttpContext } from '@adonisjs/core/http'
 import User from '#models/user'
 
 export default class UsersController {
-  public async index() {
-    const user = await User.all()
+  public async index({ request }: HttpContext) {
+    const user = await User.findBy('email', request.param('username'))
 
-    return user
+    return { username: user?.username, email: user?.email }
   }
   public async store({ request }: HttpContext) {
     const user = new User()
