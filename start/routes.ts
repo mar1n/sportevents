238f3681fa4b17f1beffd601aac2aa72ae057cb1
mar1n <30738216+mar1n@users.noreply.github.com>
2024-11-10 19:17:20 +0000

    auth controller

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,7 @@
 |
 */
 
+const AuthController = () => import('#controllers/auth_controller')
 import router from '@adonisjs/core/services/router'
 const UsersController = () => import('#controllers/users_controller')
 router.get('/', async () => {
