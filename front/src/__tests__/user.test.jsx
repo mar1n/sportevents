f2fd0943d16eeebb86159dfd906e89dcb7ea1b78
mar1n <30738216+mar1n@users.noreply.github.com>
2024-11-12 23:43:49 +0000

    test: user see  success message after click

--- front/src/__tests__/user.test.jsx
+++ front/src/__tests__/user.test.jsx
@@ -1,14 +1,16 @@
 import '@testing-library/dom'
 import { render, screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
 //import { act } from 'react'
 //import { server } from '../msw/node'
 //import { http, HttpResponse } from 'msw'
 import SignUp from '../pages/user/signUp'
 
 describe('User', () => {
-    test('User register journey success.', () => {
+    test('User register journey success.', async () => {
         render(<SignUp/>)
-    
+        const userEvents = userEvent.setup()
+
         const headLine = screen.getByText('Sign Up Page')
         expect(headLine).toBeInTheDocument()
         const signUpForm = screen.getByRole('form', {name: 'signUp'})
@@ -19,6 +21,10 @@ describe('User', () => {
         expect(passwordInput).toBeInTheDocument()
         const buttonSignUp = screen.getByRole('button', {name: 'signUp'})
         expect(buttonSignUp).toBeInTheDocument()
-        
+
+        await userEvents.click(buttonSignUp);
+        const successMessage = screen.getByText('Check your email please.')
+        expect(successMessage).toBeInTheDocument()
+
     });
 })
--- front/src/pages/user/signUp.tsx
+++ front/src/pages/user/signUp.tsx
@@ -1,14 +1,22 @@
-const SignUp = () => {
+import React, { FC, useState } from "react";
+const SignUp: FC = () => {
+    const [message, setMessage] = useState(false)
+
+    const register = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault()
+        setMessage(true)
+    }
     return <>
     <div>
         <h1>Sign Up Page</h1>
         <form aria-label="signUp">
             <input placeholder="emailSignUp" type="email" />
             <input placeholder="passwordSignUp" type="password" />
-            <button aria-label="signUp">
+            <button aria-label="signUp" onClick={(e) => register(e)}>
                 Register
             </button>
         </form>
+        {message && <p>Check your email please.</p>}
     </div>
     </>
 }
