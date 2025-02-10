bda2eb6fa317e1e6382af9b51d203c030ff1f9be
mar1n <cykcykacz@gmail.com>
2025-02-10 19:30:22 +0000

    create invalid test for Events

--- app/validators/event.ts
+++ app/validators/event.ts
@@ -2,7 +2,7 @@ import vine from '@vinejs/vine'
 
 export const createEventValidator = vine.compile(
   vine.object({
-    title: vine.string(),
-    discription: vine.string(),
+    title: vine.string().minLength(5).maxLength(255),
+    discription: vine.string().minLength(5),
   })
 )
--- tests/functional/event_controller.spec.ts
+++ tests/functional/event_controller.spec.ts
@@ -1,25 +1,28 @@
 import { test } from '@japa/runner'
 import Events from '#models/event'
 
+async function loginHelper(client: any) {
+  const userRegister = {
+    username: 'Szymon Dawidowicz',
+    email: 'szymon@fastmail.com',
+    password: 'qwertyuio',
+  }
+  await client.post('/users/register').json(userRegister)
+  const userLogin = {
+    email: 'szymon@fastmail.com',
+    password: 'qwertyuio',
+  }
+  const response = await client.get('/auth/login').qs(userLogin)
+  response.assertStatus(200)
+  response.assertBodyContains({
+    message: 'Valid credentials',
+  })
+
+  return response.headers()['set-cookie']
+}
 test.group('Events controller', () => {
   test('Create Event for Login User', async ({ client, assert }) => {
-    const userRegister = {
-      username: 'Szymon Dawidowicz',
-      email: 'szymon@fastmail.com',
-      password: 'qwertyuio',
-    }
-    await client.post('/users/register').json(userRegister)
-    const userLogin = {
-      email: 'szymon@fastmail.com',
-      password: 'qwertyuio',
-    }
-    const response = await client.get('/auth/login').qs(userLogin)
-    response.assertStatus(200)
-    response.assertBodyContains({
-      message: 'Valid credentials',
-    })
-
-    const cookie = response.headers()['set-cookie']
+    const cookie = await loginHelper(client)
     const event = {
       title: 'Even Title',
       discription: 'My discription of event...',
@@ -32,5 +35,17 @@ test.group('Events controller', () => {
 
     const findEvent = await Events.findByOrFail('title', event.title)
     assert.equal(findEvent.title, event.title)
+  })
+  test('Create invalid Event for Login User', async ({ client, assert }) => {
+    const cookie = await loginHelper(client)
+    const event = {
+      title: '',
+      discription: 'My discription of event...',
+    }
+    const eventRoute = await client.post('/events').json(event).header('Cookie', cookie)
+    eventRoute.assertStatus(422)
+    eventRoute.assertBody({
+      errors: [{ message: 'The title field must be defined', field: 'title', rule: 'required' }],
+    })
   })
 })
