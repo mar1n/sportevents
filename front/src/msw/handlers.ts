63a1a511d068c88a58d3485dade0a0aace81d315
mar1n <cykcykacz@gmail.com>
2025-05-08 21:48:59 +0100

    remove unused mock end points

--- front/src/msw/handlers.ts
+++ front/src/msw/handlers.ts
@@ -1,22 +1,6 @@
 import { http, HttpResponse } from 'msw'
 import { User } from '../pages/users/register'
 export const handlers = [
-  http.post('http://localhost:3001/users/register', () =>
-    HttpResponse.json({
-      message: 'Check your email please.',
-    })
-  ),
-  http.get('http://localhost:3001/register', () =>
-    HttpResponse.json({
-      message: 'Check your email please.',
-    })
-  ),
-  http.get('http://localhost:3001/users/:username', () =>
-    HttpResponse.json({ username: 'Szymon', email: 'cykcykacz@gmail.com' })
-  ),
-  http.post('http://localhost:3000/user/register', () => {
-    HttpResponse.json({ message: 'User created' })
-  }),
   http.post('http://localhost:6666/users/register', async ({ request }) => {
     const user = (await request.json()) as User
     return HttpResponse.json(
