5b2865048ebad0b8d6e03567c66083e442b809ae
mar1n <cykcykacz@gmail.com>
2025-07-04 21:10:49 +0100

    function for server names

--- front/src/utils/helper.ts
+++ front/src/utils/helper.ts
@@ -2,13 +2,13 @@ const setUrl = {
   production: process.env.NEXT_PUBLIC_SERVER_PROD,
   mockSerever: process.env.NEXT_PUBLIC_SERVER_MOCK,
   development: process.env.NEXT_PUBLIC_SERVER_DEV,
-  selectDomainName: function (domainName: string) {
-    if (domainName === 'production') {
-      return this.production
-    } else if (domainName === 'mockServer') {
-      return this.mockSerever
-    } else {
-      return this.development
+  getURL: function () {
+    if (process.env.NEXT_PUBLIC_SELECT_SERVER === 'production') {
+      return process.env.NEXT_PUBLIC_SERVER_PROD
+    } else if (process.env.NEXT_PUBLIC_SELECT_SERVER === 'mockServer') {
+      return process.env.NEXT_PUBLIC_SERVER_MOCK
+    } else if (process.env.NEXT_PUBLIC_SELECT_SERVER === 'development') {
+      return process.env.NEXT_PUBLIC_SERVER_DEV
     }
   },
 }
