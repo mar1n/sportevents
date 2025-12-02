b90008bc0f7eb30070f72800ff8faddc8bf7b28a
mar1n <cykcykacz@gmail.com>
2025-12-02 23:01:37 +0000

    account page css headers

--- front/src/pages/users/account.tsx
+++ front/src/pages/users/account.tsx
@@ -19,12 +19,14 @@ const Account: NextPage =()=> {
   }, [])
   return (
     <>
-      <div>
-        <h1>Account Page</h1>
-        <h2>Name: {user}</h2>
+      <div  className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h1 className='mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900'>Account Page</h1>
+        <h2 className='mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900'>Name: {user}</h2>
       </div>
       <div>
-        <Link href="/events/createevent">CreateEvent</Link>
+        <Link className='text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none' href="/events/createevent">
+        <a className='text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none'></a>
+        CreateEvent</Link>
         <Link href="/events/displayevents">DisplayEvents</Link>
         <Link href="/events/myevents">MyEvents</Link>
         <Link href="/events/attendevents">AttendEvents</Link>
